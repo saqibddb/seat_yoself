3518f1845ceb35f7aef6536239288b124a7d1d10
Jeremy Tupper <jeremytupper1@gmail.com>
2015-02-11 16:43:23 -0500

    Change controllers, add admin controllers

---
+++ app/controllers/admin/reservations_controller.rb
@@ -0,0 +1,24 @@
+class Admin::ReservationsController < Admin::ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to restaurants_url, notice: "Logged in!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render "new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to restaurants_url, notice: "Logged out!"
   end
 end
