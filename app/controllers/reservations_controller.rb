d3478144768be6e8dd93ff157028c1b878cb4d20
Jeremy Tupper <jeremytupper1@gmail.com>
2015-02-12 10:02:34 -0500

    Add views for reservations and admin

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,24 @@
 class ReservationsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
